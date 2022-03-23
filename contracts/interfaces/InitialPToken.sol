c4e4c4af5187fa677bab82952217232c85cd4eb7
Mars chen <MarsChenSE@gmail.com>
2022-03-23 15:32:25 +0800

    Create the initial interface for tokens

--- contracts/interfaces/InitialPToken.sol
+++ contracts/interfaces/InitialPToken.sol
@@ -1,52 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import {CounterInterface} from "./CounterInterface.sol";
+import {CounterInterface} from "../CounterInterface.sol";
 import {IncentivesController} from "./Incentives.sol";
 
 /**
- * @title IInitializableDebtToken
- * @notice Interface for the initialize function common between debt tokens
- * @author Aave
+ * @title IInitialPToken
+ * @notice Interface for the initialize function on PToken
+ * @author Prestare
  **/
 interface InitialPToken {
+
     /**
-    * @dev Emitted when a debt token is initialized
-    * @param underlyingAsset The address of the underlying asset
-    * @param pool The address of the associated lending pool
-    * @param incentivesController The address of the incentives controller for this pToken
-    * @param debtTokenDecimals the decimals of the debt token
-    * @param debtTokenName the name of the debt token
-    * @param debtTokenSymbol the symbol of the debt token
-    * @param params A set of encoded parameters for additional initialization
-    **/
-    event Initialized(
-    address indexed underlyingAsset,
-    address indexed pool,
-    address incentivesController,
-    uint8 debtTokenDecimals,
-    string debtTokenName,
-    string debtTokenSymbol,
-    bytes params
+     * @dev Emitted after the initialization. 
+     * @param counter The address of the counter where this pToken will be used
+     * @param gasStation The address of the Aave treasury, receiving the fees on this pToken
+     * @param underlyingAsset The address of the underlying asset of this pToken (E.g. WETH for aWETH)
+     * incentivesController The smart contract managing potential incentives distribution
+     * @param pTokenDecimals The decimals of the pToken, same as the underlying asset's
+     * @param pTokenName The name of the pToken
+     * @param pTokenSymbol The symbol of the pToken
+     */
+    event Initialized(        
+        address counter,
+        address gasStation,
+        address underlyingAsset,
+        address crt
+        uint8 pTokenDecimals,
+        string callData pTokenName,
+        string callData pTokenSymbol,
+        bytes calldata params
     );
 
     /**
-    * @dev Initializes the debt token.
-    * @param pool The address of the lending pool where this pToken will be used
-    * @param underlyingAsset The address of the underlying asset of this pToken (E.g. WETH for aWETH)
-    * @param incentivesController The smart contract managing potential incentives distribution
-    * @param debtTokenDecimals The decimals of the debtToken, same as the underlying asset's
-    * @param debtTokenName The name of the token
-    * @param debtTokenSymbol The symbol of the token
-    */
+     * @notice Initializes the PToken 
+     * @param counter The address of the counter where this pToken will be used
+     * @param gasStation The address of the Prestare treasury, receiving the fees on this pToken
+     * @param underlyingAsset The address of the underlying asset of this pToken (E.g. WETH for aWETH)
+     * incentivesController The smart contract managing potential incentives distribution
+     * @param pTokenDecimals The decimals of the pToken, same as the underlying asset's
+     * @param pTokenName The name of the pToken
+     * @param pTokenSymbol The symbol of the pToken
+     */
+    // maybe we should add a initializer modify to check the status before call this function
     function initialize(
-    CounterInterface pool,
-    address underlyingAsset,
-    IncentivesController incentivesController,
-    uint8 debtTokenDecimals,
-    string memory debtTokenName,
-    string memory debtTokenSymbol,
-    bytes calldata params
+        PCOUNTER counter,
+        address gasStation,
+        address underlyingAsset,
+        address crt
+        uint8 pTokenDecimals,
+        string callData pTokenName,
+        string callData pTokenSymbol,
+        bytes calldata params
     ) external;
 }
 
