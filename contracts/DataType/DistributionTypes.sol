84faa7672166dc10050b97fffdf23b1822dd6635
Mars chen <MarsChenSE@gmail.com>
2022-03-30 17:42:00 +0800

    Add DistributionType

---
+++ contracts/DataType/DistributionTypes.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.4;
+
+library DistributionTypes {
+  struct AssetConfigInput {
+    uint104 emissionPerSecond;
+    uint256 totalStaked;
+    address underlyingAsset;
+  }
+
+  struct UserStakeInput {
+    address underlyingAsset;
+    uint256 stakedByUser;
+    uint256 totalStaked;
+  }
+}
