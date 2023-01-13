817785f6f95ede1bc9ed8d89e6328542437286f4
Mars chen <MarsChenSE@gmail.com>
2023-01-13 19:24:09 +0800

    add WETH gateway

---
+++ contracts/interfaces/IWETHGateway.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+interface IWETHGateway {
+  function depositETH(
+    address lendingPool,
+    address onBehalfOf,
+    uint16 referralCode
+  ) external payable;
+
+  function withdrawETH(
+    address lendingPool,
+    uint256 amount,
+    address onBehalfOf
+  ) external;
+
+  function repayETH(
+    address lendingPool,
+    uint256 amount,
+    uint256 rateMode,
+    address onBehalfOf
+  ) external payable;
+
+  function borrowETH(
+    address lendingPool,
+    uint256 amount,
+    uint256 interesRateMode,
+    uint16 referralCode,
+    bool crtenable
+  ) external;
+}
