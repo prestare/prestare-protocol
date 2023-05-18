c1c45c8afdaca4261b99765bd797be5a2d20efc4
MarsChenSE <MarsChenSE@gmail.com>
2023-05-18 14:22:03 +0800

    add comment on transferToken4Test

--- scripts/transferToken4Test.ts
+++ scripts/transferToken4Test.ts
@@ -2,17 +2,20 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { getPTokenContract, getTokenContract } from "../helpers/contracts-getter";
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
-import { aDAIHolder, USDCHolder } from "../helpers/holder";
+import { DAIHolder, USDCHolder } from "../helpers/holder";
 import { ethers } from "hardhat";
 const hre: HardhatRuntimeEnvironment = require('hardhat');
 
 async function main() {
     console.log("Test paToken's balanceOf is equal to the amount of the underlying asset instead of atoken");
 
+    // 修改不同的代币持有者
     await impersonateAccount(USDCHolder);
     const fakeSigner: SignerWithAddress = await hre.ethers.getSigner(USDCHolder);
+    // 代币名字
     let tokenSymbol = 'USDC';
-    let amount = '200';
+    let amount = '1000';
+    // 修改dicimals
     let inputAmount = ethers.utils.parseUnits(amount, 6);
 
     // await depositToken(fakeSigner, tokenSymbol, amount);
@@ -20,7 +23,6 @@ async function main() {
     let Token = await getTokenContract(tokenSymbol);
     let pToken = await getPTokenContract(tokenSymbol);
 
-    // 
     console.log("%s balance: ", tokenSymbol, await Token.balanceOf(to));
 
     await Token.connect(fakeSigner).transfer(to, inputAmount);
