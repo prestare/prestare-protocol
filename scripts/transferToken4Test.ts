7f6ac332529709b7d6f60ed4ca89070e6b0b6b2d
MarsChenSE <MarsChenSE@gmail.com>
2023-05-30 23:13:54 +0800

    add ETH transfer

--- scripts/transferToken4Test.ts
+++ scripts/transferToken4Test.ts
@@ -2,7 +2,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { getPTokenContract, getTokenContract } from "../helpers/contracts-getter";
 import { impersonateAccount } from "@nomicfoundation/hardhat-network-helpers";
-import { DAIHolder, USDCHolder } from "../helpers/holder";
+import { DAIHolder, USDCHolder, ETHHolder } from "../helpers/holder";
 import { ethers } from "hardhat";
 const hre: HardhatRuntimeEnvironment = require('hardhat');
 
@@ -10,23 +10,33 @@ async function main() {
     console.log("Test paToken's balanceOf is equal to the amount of the underlying asset instead of atoken");
 
     // 修改不同的代币持有者
-    await impersonateAccount(USDCHolder);
-    const fakeSigner: SignerWithAddress = await hre.ethers.getSigner(USDCHolder);
+    await impersonateAccount(ETHHolder);
+    const fakeSigner: SignerWithAddress = await hre.ethers.getSigner(ETHHolder);
     // 代币名字
-    let tokenSymbol = 'USDC';
-    let amount = '1000';
+    let tokenSymbol = 'ETH';
+    let amount = '100';
     // 修改dicimals
-    let inputAmount = ethers.utils.parseUnits(amount, 6);
+    let inputAmount = ethers.utils.parseUnits(amount, 18);
 
-    // await depositToken(fakeSigner, tokenSymbol, amount);
-    let to = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-    let Token = await getTokenContract(tokenSymbol);
-    let pToken = await getPTokenContract(tokenSymbol);
+    let to = "0xD5A832A6813631380b72C503c980a4d2B30934F1";
 
-    console.log("%s balance: ", tokenSymbol, await Token.balanceOf(to));
+    // transfer ERC20 token
+    // let Token = await getTokenContract(tokenSymbol);
+    // let pToken = await getPTokenContract(tokenSymbol);
 
-    await Token.connect(fakeSigner).transfer(to, inputAmount);
-    console.log("%s balance: ", tokenSymbol, await Token.balanceOf(to));
+    // console.log("%s balance: ", tokenSymbol, await Token.balanceOf(to));
+
+    // await Token.connect(fakeSigner).transfer(to, inputAmount);
+    // console.log("%s balance: ", tokenSymbol, await Token.balanceOf(to));
+
+    // transfer ETH
+    const tx = {
+        to: to,
+        value: ethers.utils.parseEther("100")
+    }
+    const receipt = await fakeSigner.sendTransaction(tx)
+    await receipt.wait() // 等待链上确认交易
+    console.log(receipt)
     
 }
 
